9c445cbb4a5eeb2a26283b63227f87b70c02ee4f
Rizki Bagus Pangestu <akubagus1108@gmail.com>
2023-11-12 09:59:36 +0700

    Sidebar

--- resources/views/bo/partial/sidebar.blade.php
+++ resources/views/bo/partial/sidebar.blade.php
@@ -3,7 +3,7 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
         <li class="nav-item">
-            <a class="nav-link {{ ($title == "Dashboard") ? '' : 'collapsed' }}" href="/">
+            <a class="nav-link {{ ($title == "Dashboard") ? '' : 'collapsed' }}" href="/admin/dashboard">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
@@ -32,28 +32,20 @@
                 </a>
             </li><!-- End Surat Masuk Nav -->
         @endcan
-        <li class="nav-heading">Pages</li>
 
         <li class="nav-item">
-            <a class="nav-link {{ ($title == "Profile") ? '' : 'collapsed' }}" href="/profile">
+            <a class="nav-link {{ ($title == "Data Penduduk") ? '' : 'collapsed' }}" href="/penduduk">
                 <i class="bi bi-person"></i>
-                <span>Profile</span>
+                <span>Penduduk</span>
             </a>
-        </li><!-- End Profile Page Nav -->
+        </li><!-- End Penduduk Page Nav -->
 
         <li class="nav-item">
-            <a class="nav-link {{ ($title == "Register") ? '' : 'collapsed' }}" href="/register">
-                <i class="bi bi-card-list"></i>
-                <span>Register</span>
+            <a class="nav-link {{ ($title == "Data Penduduk Migrasi") ? '' : 'collapsed' }}" href="/penduduk-migrasi">
+                <i class="bi bi-person"></i>
+                <span>Penduduk Migrasi</span>
             </a>
-        </li><!-- End Register Page Nav -->
-
-        <li class="nav-item">
-            <a class="nav-link {{ ($title == "Login") ? '' : 'collapsed' }}" href="/login">
-                <i class="bi bi-box-arrow-in-right"></i>
-                <span>Login</span>
-            </a>
-        </li><!-- End Login Page Nav -->
+        </li><!-- End Penduduk Migrasi Page Nav -->
 
     </ul>
 
