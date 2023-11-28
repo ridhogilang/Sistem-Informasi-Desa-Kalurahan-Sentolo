6811f35f4c9c48e8d3020d21a0b03822291a7e82
Rizki Bagus Pangestu <akubagus1108@gmail.com>
2023-11-28 18:25:08 +0700

    surat masuk update

--- routes/web.php
+++ routes/web.php
@@ -359,8 +359,8 @@ Route::prefix('admin')->group(function () {
             Route::get('/surat-masuk', [SMasukController::class, 'index'])->middleware('can:Surat Masuk');
             Route::post('/surat-masuk', [SMasukController::class, 'store'])->middleware('can:Surat Masuk');
             Route::put('/surat-masuk/{id}', [SMasukController::class, 'update'])->middleware('can:Surat Masuk');
-            Route::get('/surat-masuk/{id}/view', [SMasukController::class, 'show'])->middleware('can:Surat Masuk');
-            Route::get('/surat-masuk/{id}/document', [SMasukController::class, 'document'])->middleware('can:Surat Masuk');
+            Route::get('/surat-masuk/{id}/document', [SMasukController::class, 'show'])->middleware('can:Surat Masuk');
+            // Route::get('/surat-masuk/{id}/document', [SMasukController::class, 'document'])->middleware('can:Surat Masuk');
             Route::delete('/surat-masuk/{id}/delete', [SMasukController::class, 'destroy'])->middleware('can:Surat Masuk');
         });
         //untuk tim sistem informasi
